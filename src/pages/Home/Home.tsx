e9c2f27afeaa3a10b827f75c341ed53edf1e86ba
Arsenyuk Danil <90194057+sl14888@users.noreply.github.com>
2022-05-13 15:22:49 +0300

    настроил роуты для проекта

---
+++ src/pages/Home/Home.tsx
@@ -0,0 +1,13 @@
+import React from 'react'
+
+import s from './Home.module.scss'
+
+type Props = {}
+
+const Home = (props: Props) => {
+  return (
+    <div className={s.home}>Home</div>
+  )
+}
+
+export default Home
---
+++ src/pages/MonthStatistics/MonthStatistics.tsx
@@ -0,0 +1,12 @@
+import React from 'react'
+import './MonthStatistics.module.scss'
+
+type Props = {}
+
+const MonthStatistics = (props: Props) => {
+  return (
+    <div>MonthStatistics</div>
+  )
+}
+
+export default MonthStatistics
