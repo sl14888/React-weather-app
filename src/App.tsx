df326ad3ce077ef99fe41e89ca1dc00d33774b57
Arsenyuk Danil <90194057+sl14888@users.noreply.github.com>
2022-05-13 23:55:21 +0300

    стили для селектора

--- src/App.tsx
+++ src/App.tsx
@@ -1,15 +1,17 @@
 import React from 'react';
 import { Routes, Route } from 'react-router-dom';
 import Home from './pages/Home/Home';
+import Header from './shared/Header/Header'
 import MonthStatistics from './pages/MonthStatistics/MonthStatistics';
 import './styles/index.scss'
 
 function App() {
   return (
-    <div className="App">
+    <div className="container">
+      <Header />
       <Routes>
-       <Route path="/" element={<Home />}/>
-       <Route path="month-statistics" element={<MonthStatistics />} />
+        <Route path="/" element={<Home />} />
+        <Route path="month-statistics" element={<MonthStatistics />} />
       </Routes>
     </div>
   );
---
+++ src/shared/Header/Header.tsx
@@ -0,0 +1,42 @@
+import React from 'react'
+import GlobalSvgSelector from '../../assets/icons/global/GlobalSvgSelector'
+import s from './Header.module.scss'
+
+import Select from 'react-select'
+
+
+type Props = {}
+
+const Header = (props: Props) => {
+    const options = [
+        { value: 'city-1', label: 'Краснодар' },
+        { value: 'city-2', label: 'Москва' },
+        { value: 'city-3', label: 'Спб' }
+    ]
+    const colourStyles = {
+        control: (styles: any) => ({
+            ...styles,
+            backgroundColor: 'rgba(71, 147, 255, 0.2)',
+            width: '194px',
+            height: '37px',
+            border: 'none',
+            borderRadius: '10px',
+            zIndex: 100,
+        }),
+    };
+    return (
+        <header className={s.header}>
+            <div className={s.wrapper}>
+                <div className={s.logo}><GlobalSvgSelector id='header-logo' /></div>
+                <div className={s.title}>React weather</div>
+            </div>
+            <div className={s.wrapper}>
+                <div className={s.change_theme}>
+                    <GlobalSvgSelector id='change-theme' />
+                </div>
+                <Select defaultValue={options[0]} styles={colourStyles} options={options} />
+            </div>
+        </header>
+    )
+}
+export default Header;
