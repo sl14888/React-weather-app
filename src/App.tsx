645120011ac4a5b8c7f9a64b8b7695e85036a239
Arsenyuk Danil <90194057+sl14888@users.noreply.github.com>
2022-05-17 00:13:56 +0300

    создан компонент попапа

--- src/App.tsx
+++ src/App.tsx
@@ -4,10 +4,12 @@ import Home from './pages/Home/Home';
 import Header from './shared/Header/Header'
 import MonthStatistics from './pages/MonthStatistics/MonthStatistics';
 import './styles/index.scss'
+import Popup from './shared/Popup/Popup';
 
 function App() {
   return (
     <div className="container">
+      <Popup />
       <Header />
       <Routes>
         <Route path="/" element={<Home />} />
---
+++ src/shared/Popup/Popup.tsx
@@ -0,0 +1,42 @@
+import React from 'react'
+import { Item } from '../../pages/Home/components/ThisDayInfo/ThisDayInfo'
+import ThisDayItem from '../../pages/Home/components/ThisDayInfo/ThisDayItem'
+import s from './Popup.module.scss'
+
+interface Props { }
+
+const Popup = ({ }: Props) => {
+    const items = [
+        {
+            icon_id: 'temp',
+            name: 'Температура',
+            value: '20° - ощущается как 17°',
+        },
+        {
+            icon_id: 'pressure',
+            name: 'Давление',
+            value: '765 мм ртутного столба - нормальное',
+        },
+        {
+            icon_id: 'precipitation',
+            name: 'Осадки',
+            value: 'Без осадков',
+        },
+        {
+            icon_id: 'wind',
+            name: 'Ветер',
+            value: '3 м/с юго-запад - легкий ветер',
+        },
+    ];
+    return (
+        <div className={s.popup}>
+            <div className={s.this_day_info_items}>
+                {items.map((item: Item) => (
+                    <ThisDayItem key={item.icon_id} item={item} />
+                ))}
+            </div>
+        </div>
+    )
+}
+
+export default Popup
